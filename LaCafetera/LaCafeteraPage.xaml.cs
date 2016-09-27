29692155d69ecfa6be623510d8d2c5600a8edceb
Alberto Gonzalez <dev@albertogonzalez.net>
2016-09-27 21:23:40 +0100

    add textbox and button for playing an audio stream from an URL

--- LaCafetera/LaCafeteraPage.xaml.cs
+++ LaCafetera/LaCafeteraPage.xaml.cs
@@ -1,12 +1,46 @@
 ﻿using Xamarin.Forms;
+using System;
+using Foundation;
+using UIKit;
+
+using StreamingAudio;
 
 namespace LaCafetera
 {
 	public partial class LaCafeteraPage : ContentPage
 	{
+		StreamingAudioManager streamingAudioManager;
+
 		public LaCafeteraPage()
 		{
 			InitializeComponent();
+			statusLabel.Text = "";
+			streamingAudioManager = new StreamingAudioManager();
+			streamingAudioManager.downloadProgressDelegate = new StreamingAudioManager.DownloadProgressDelegate(this.OnDownloadProgress);
+			streamingAudioManager.ErrorOccurred += HandleStreamingAudioError;
+		}
+
+		void OnPlay(object sender, EventArgs e)
+		{
+			string streamURL = streamURLEntry.Text;
+			streamingAudioManager.Play(streamURL, PlayerOption.Stream);
+		}
+
+		void OnDownloadProgress(float progress)
+		{
+			Device.BeginInvokeOnMainThread(() =>
+			{
+				statusLabel.Text = "downloaded " + (((int)(progress * 10000)) / 100.0f) + "%";
+			});
+		}
+
+		void HandleStreamingAudioError(object sender, StreamingAudioErrorEventArgs error)
+		{
+			Device.BeginInvokeOnMainThread(() =>
+			{
+				//statusLabel.Text = error.Description;
+				statusLabel.Text += "\n" + error.Description;
+			});
 		}
 	}
 }
